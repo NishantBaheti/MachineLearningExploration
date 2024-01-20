82a2ed915060ea9f44bfa97444e95c16e4b7811f
Nishant Baheti <nishantbaheti.it19@gmail.com>
2024-01-20 12:49:02 +0000

    sphinx doc issue

--- conf.py
+++ conf.py
@@ -53,8 +53,9 @@ html_static_path = ['_static']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
+
+# include_patterns = [ "*.rst" ]
 exclude_patterns = ['_build', '**.ipynb_checkpoints','docs']
-include_patterns = ['.ipynb', ".rst", ".md"]
 
 master_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
